f27faae96a22be6b5e215983429c724f8ddba04f
Jason Walsh <jwalsh@saylent.com>
2017-06-12 09:30:21 -0400

    Training tutorial scaffolding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import * as NgCore from  '@angular/core';
+
+console.log(NgCore)
+
 import { AppComponent } from './app.component';
+import { NewCmpComponent } from './feature/new-cmp/new-cmp.component';
+import { MyNewComponentComponent } from './my-new-component/my-new-component.component';
+import { MyNewDirectiveDirective } from './my-new-directive.directive';
+import { MyNewPipePipe } from './my-new-pipe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewCmpComponent,
+    MyNewComponentComponent,
+    MyNewDirectiveDirective,
+    MyNewPipePipe
   ],
   imports: [
     BrowserModule
---
+++ src/app/new-module/new-module.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NewComponentComponent } from './new-component/new-component.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [NewComponentComponent]
+})
+export class NewModuleModule { }
